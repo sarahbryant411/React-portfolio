2af0f9514a1c342c60ace48dc6a3b5138dcfea2a
Sarahbryant411 <sarahbryant411@gmail.com>
2024-03-25 17:44:46 -0400

    added the highlight to the header nav bar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
 function Footer() {
   return (
     <footer>
-      <p>© 2024</p>
+      <a href='www.linkedin.com/in/sarah-bryant572173' target='_blank' rel='noreferrer'>LinkedIn</a>
+      <a href='https://github.com/sarahbryant411' target='_blank' rel='noreferrer'>GitHub</a>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Header() {
+  const location = useLocation();
   return (
     <header>
       <h1>Sarah Bryant</h1>
       <ul>
-        <li>< Link to="/">About</Link></li>
-        <li>< Link to="/portfolio">Portfolio</Link></li>
-        <li>< Link to="/contact">Contact</Link></li>
-        <li>< Link to="/resume">Resume</Link></li>
+        <li>< Link className={location.pathname==="/" && "active"} to="/">About</Link></li>
+        <li>< Link className={location.pathname==="/portfolio" && "active"} to="/portfolio">Portfolio</Link></li>
+        <li>< Link className={location.pathname==="/contact" && "active"}to="/contact">Contact</Link></li>
+        <li>< Link className={location.pathname==="/resume" && "active"}to="/resume">Resume</Link></li>
       </ul>
     </header>
   );
